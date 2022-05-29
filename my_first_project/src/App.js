015b73e523826a6622db3662fdac0df42e31f92e
Kochiev Oleg <developer-artisan@mail.ru>
2022-05-29 12:41:39 +0300

    was added first lesson

---
+++ my_first_project/src/App.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      <Tick />
+      <Hello name="Jhon"/>
+      <Comment author={{
+        name: 'Jhon',
+        avatarUrl: './title.com',
+        text: 'Lorem ipsum...'
+      }} />
+      </header>
+    </div>
+  );
+}
+
+class Hello extends React.Component {
+  render(props) {
+    return <h1>Hello { this.props.name } !</h1>
+  }
+}
+
+
+function Tick(props) {
+  const element = (
+    <div>
+      <h1>Hi!</h1>
+      <h2>It is {new Date().toLocaleString()}</h2>
+    </div>
+  );
+  return element;
+}
+
+class Avatar extends React.Component {
+  render() {
+    return (
+      <img className="Avatar"      
+        src={this.props.user.avatarUrl}      
+        alt={this.props.user.name} 
+      />
+    );
+  }
+}
+
+class UserInfo extends React.Component {
+  render() {
+    return (
+      <div className="UserInfo">
+        <Avatar user={ this.props.user } />
+        <div className="UserInfo-name">
+          { this.props.author.name }
+        </div>             
+      </div>
+    );
+  } 
+}
+
+class Comment extends React.Component {
+  render() {
+    return (
+      <div className="Comment">
+        <UserInfo user={ this.props.user }
+          author={ this.props.author }
+        />
+        <div className="Comment-text">
+          {this.props.author.text}
+        </div>
+        <div className="Comment-date">
+          { this.props.author.date }
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+export {
+  App,
+  Tick,
+  Comment
+};
+
+/* 
+{{
+  avatarUrl: 'https://someurl/com',
+  name: 'Harry'}}
+  author={{
+    text:"Lorem Ipsum",
+    date: new Date().toLocaleString()
+  } */
---
+++ my_first_project/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// import { App, Comment } from './App';
+import Clock from './Time';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    {/* <App />, */}
+    {/* <Comment user={{
+      avatarUrl: 'https://someurl/com',
+      name: 'Harry'
+    }}
+      author={{
+        text:"Lorem Ipsum",
+        date: new Date().toLocaleString(),
+        name: "Harry"
+      }}
+    /> */}
+    <Clock timer={{ time: 1100 }}/>
+    <Clock timer={{ time: 1300 }}/>
+    <Clock timer={{ time: 1600 }}/>
+    <Clock timer={{ time: 1800 }}/>
+  </React.StrictMode>
+);
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
