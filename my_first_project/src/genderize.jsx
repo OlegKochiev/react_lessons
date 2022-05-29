b58ef41203c62b14d314a2d2986d7cc28e5b85aa
Kochiev Oleg <developer-artisan@mail.ru>
2022-05-29 14:50:50 +0300

    was added bubbling for inputValue and male request

--- my_first_project/src/genderize.jsx
+++ my_first_project/src/genderize.jsx
@@ -1,18 +1,6 @@
 import React from "react";
 import './genderize.css';
 
-class Input extends React.Component {
-  constructor(props) {
-    super(props);
-    this.input = document.getElementById('input');
-  }
-  render() {
-    return (
-      <input type="text" id="input"></input>
-    )
-  } 
-}
-
 class Button extends React.Component {
   render() {
     return (
@@ -21,25 +9,59 @@ class Button extends React.Component {
   } 
 }
 
-class Form extends React.Component {
-
+class Input extends React.Component {
   constructor(props) {
     super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = { inputValue: '' };
+    this.handleKeyUp = this.handleKeyUp.bind(this);
   }
 
-  handleSubmit (e)  {
-    console.log('hello submit');
-    console.log(this);
+  handleKeyUp(e) {
+    this.props.onInputValueChange(e.target.value);
+  }
+
+  render() {
+    return (
+      <input type="text" id="input" onKeyUp={ this.handleKeyUp }></input>
+    )
+  } 
+}
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { inputValue: '' }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputKeyUp = this.handleInputKeyUp.bind(this);
+  }
+
+  handleInputKeyUp(inputValue) {
+    this.setState({
+      inputValue: inputValue
+    });
+  }
+
+  async handleSubmit (e)  {
     e.preventDefault();
+    let sex = await this.handleRequest();
+    console.log(sex);
+  }
+
+  async handleRequest() {
+    const firstName = this.state.inputValue;
+    const serverUrl = 'https://api.genderize.io';
+    const url = `${ serverUrl }?name=${ firstName }`;
+    const response = await fetch(url);
+    const sex = await response.json();
+    return sex;
   }
 
   render() {
     return (
       <form action="#" method="POST" id="form" onSubmit={ this.handleSubmit }>
         <label>
-          { "Hi label" }
-          <Input /> 
+          { this.state.inputValue } 
+          <Input onInputValueChange={ this.handleInputKeyUp } /> 
         </label>
         <Button />
       </form>
